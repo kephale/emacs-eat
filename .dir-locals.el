9eadacb243f1deaafe71fdaa8c5b95374f1749f1
Akib Azmain Turja <akib@disroot.org>
2022-11-26 22:08:52 +0600

    ; * .dir-locals.el: No tabs and 70 column limit

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((sh-mode . ((sh-basic-offset . 2))))
+((nil . ((fill-column . 70)
+         (indent-tabs-mode . nil)))
+ (sh-mode . ((sh-basic-offset . 2))))
